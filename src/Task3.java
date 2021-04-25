ce3231aefbf008171df65807d9dd0c593d3f0fbd
pasatigiy <82204580+pasatigiy@users.noreply.github.com>
2021-04-25 13:26:21 +0300

    Add files via upload

--- src/Task3.java
+++ src/Task3.java
@@ -1,10 +1,30 @@
-/** В этом классе необходимо перемножить матрицы двух массивов.
- * Решение данной задачи пока отложено до уточнения условия. *
+/**
+ * В этой задаче мы премножаем матрицы двух массивов. В нашем случае матрицы согласованы, т.к.
+ * число столбцов в первом множителе равно числу строк во втором, значит опреация выполнима.
  */
 public class Task3 {
     public static void main(String[] args) {
-        int[][] myArray1 = {{1, 0, 0, 0,}, {0, 1, 0, 0}, {0, 0, 0, 0}};
-        int[][] myArray2 = {{1, 2, 3}, {1, 1, 1}, {0, 0, 0}, {2, 1, 0}};
+
+        int[][] myArray1 = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}};
+        int[][] myArray2 = {{1, 2, 3}, {1, 1, 1}, { 0, 0, 0}, {2, 1, 0}};
+
+        int[][] myArray3 = new int[myArray1.length][myArray2[0].length];
+        for ( int i =0; i<myArray3[0].length; i++){
+            for ( int j = 0; j<myArray3.length; j++){
+                for ( int k = 0; k<myArray1[0].length; k++){
+                    myArray3[i][j] = myArray3[i][j] + myArray1[i][k] * myArray2[k][j];
+
+                }
+            }
+        }
+        for ( int i = 0; i<myArray3.length; i++ ){
+            for ( int j = 0; j<myArray3[0].length; j++ ){
+                System.out.print( myArray3[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
 
     }
 }
